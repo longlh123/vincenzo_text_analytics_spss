65982ffdd080112227171c604a0dfd9676bfe065
Toan Ngo <Toan.Ngo@ipsos.com>
2024-05-28 15:01:35 +0700

    first start with dev-toanngo

--- transfer_to_spss.py
+++ transfer_to_spss.py
@@ -2,5 +2,9 @@ import pandas as pd
 
 df = pd.read_excel("source\\VINCENZO 2024 - CODEFRAME - Final - updated 10052024.xlsx", engine="openpyxl",sheet_name="CODEFRAME CSAT - proccess")
 df.set_index(["LV1","LV2","LV3","LV4","LV5"], inplace=True)
+
+for i in list(df.index):
+    print(i)
+
 print(df)
 
